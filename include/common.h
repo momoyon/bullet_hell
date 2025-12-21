a8234d1a12d218a9b86a1819c05ec6f4bd0d7128
ahmedsamyh <ahmedsamyh10@gmail.com>
2025-12-22 01:32:41 +0500

    [common] Add draw_info_text()

--- include/common.h
+++ include/common.h
@@ -4,5 +4,6 @@
 #include <raylib.h>
 
 void bind(Vector2 *p, float radius, Rectangle bound);
+void draw_info_text(Vector2 *p, const char *text, int font_size, Color color);
 
 #endif // COMMON_H_
--- src/common.c
+++ src/common.c
@@ -1,8 +1,14 @@
 #include <common.h>
+#include <engine.h>
 
 void bind(Vector2 *p, float radius, Rectangle bound) {
 	if (p->x < bound.x + radius) p->x = bound.x + radius;
 	if (p->x > bound.x + bound.width - radius) p->x = bound.x + bound.width - radius;
 	if (p->y < bound.y + radius) p->y = bound.y + radius;
 	if (p->y > bound.y + bound.height - radius) p->y = bound.y + bound.height - radius;
+}
+
+void draw_info_text(Vector2 *p, const char *text, int font_size, Color color) {
+    draw_text(GetFontDefault(), text, *p, font_size, color);
+    p->y += font_size + 2;
 }
